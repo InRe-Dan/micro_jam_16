11ebd25fc171920e105ba4036d6615b22534c730
Dan D. <daniel.mi.do@gmail.com>
2024-06-15 15:18:46 +0100

    Fixed keyboard inputs affecting the in-game UI

--- game_ui/game_ui.gd
+++ game_ui/game_ui.gd
@@ -3,7 +3,11 @@ extends TabContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var queue : Array[Control] = [self]
+	while queue:
+		var control : Control = queue.pop_front()
+		queue.append_array(control.get_children())
+		control.focus_mode = control.FOCUS_NONE
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
