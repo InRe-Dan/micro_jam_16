58c46696cc9585a563294189f9c37a174c53d03e
Dan D. <daniel.mi.do@gmail.com>
2024-06-15 07:21:30 +0100

    Added (bad) healthbar

--- ship/ship.gd
+++ ship/ship.gd
@@ -3,32 +3,26 @@ class_name Ship extends RigidBody2D
 
 ## Rate at which the ship gains velocity while thrusting
 @export_range(1, 100, 1) var acceleration: int = 60
-
 ## Rate at which the ship gains rotational velocity while turning
 @export_range(0.1, 10.0, 0.1) var torque: float = 1.2
-
 ## Maximum velocity the ship is able to reach
 @export_range(0.1, 10.0, 0.1) var terminal_velocity: float = 4.0
-
 ## Maximum rotational velocity the ship is able to reach
 @export_range(0.1, 10.0, 0.1) var terminal_rotational_velocity: float = 4.0
-
 ## Braking strength
 @export_range(0.0, 1.0, 0.1) var braking_strength: float = 0.5
-
 ## Angular braking strength
 @export_range(0.0, 10.0, 0.1) var angle_braking_strength: float = 6.0
-
 ## Maximum health of the ship
 @export_range(1, 1000, 100) var maximum_health: int = 100
 
 ## Current health of the ship
 @onready var health: float = maximum_health
+@onready var camera: Camera2D = $Camera2D
+@onready var health_bar : TextureProgressBar = $HealthBar
 
 ## Ship's current rotational velocity
 var rotational_velocity: float = 0.0
-
-@onready var camera: Camera2D = $Camera2D
 
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -61,6 +55,7 @@ func damage(dmg: int) -> void:
 	health -= dmg
 	if health <= 0:
 		die()
+	health_bar.value = health / maximum_health
 
 	
 ## Returns the current viewport
