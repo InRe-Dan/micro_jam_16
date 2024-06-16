80be78cf3b55f5abadd42a77e401ca88eb39e4c3
Dan D. <daniel.mi.do@gmail.com>
2024-06-16 20:38:42 +0100

    Fix to flashing

--- game_ui/warning_label.gd
+++ game_ui/warning_label.gd
@@ -9,13 +9,13 @@ func _process(delta: float) -> void:
 		self_modulate.a = 1.0
 	else:
 		self_modulate.a = 0.0
+
 	if target > 0.5:
-		modulate.a = lerpf(modulate.a, 1.0, 0.1 * delta)
-		if modulate.a > 0.9:
-			target = 0.0
+		modulate.a = lerpf(modulate.a, 1.0, 15 * delta)
+		if modulate.a > 0.8:
+			target = 0
 	
 	if target < 0.5:
-		modulate.a = lerpf(modulate.a, 0.0, 0.1 * delta)
-		if modulate.a < 0.1:
+		modulate.a = lerpf(modulate.a, 0.0, 15 * delta)
+		if modulate.a < 0.4:
 			target = 1.0
-	
