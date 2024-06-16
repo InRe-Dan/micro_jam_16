622fe41d86ef2d69e9f7606a99fea9ea48e653f7
Grant <53729381+endgrant@users.noreply.github.com>
2024-06-16 13:46:08 -0400

    Boss changes

--- hazard/dreadnull.gd
+++ hazard/dreadnull.gd
@@ -1,11 +1,15 @@
-extends RigidBody2D
+class_name Dreadnull extends Enemy
+
+var fighting: bool = false
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta) -> void:
+	if not fighting:
+		var ship: Ship = get_tree().get_first_node_in_group("player") as Ship
+		if not ship or not is_instance_valid(ship):
+			return
+		
+		if ship.position.distance_squared_to(position) <= engagement_distance * 4:
+			fighting = true
+	else:
+		super(delta)
