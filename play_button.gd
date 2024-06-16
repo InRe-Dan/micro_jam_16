fcf1ef2e117805dee2b422fd8ec5c9137c58e29e
Dan D. <daniel.mi.do@gmail.com>
2024-06-16 21:20:55 +0100

    Worst main menu ever :muscle:

---
+++ play_button.gd
@@ -0,0 +1,9 @@
+extends Button
+
+
+func _on_pressed() -> void:
+	$"../..".visible = false
+	$"../../../Hotbar".visible = true
+	$"../../../DraggableWindow".visible = true
+	$"../../../../Pointer".visible = true
+	get_tree().current_scene.process_mode = Node.PROCESS_MODE_INHERIT
